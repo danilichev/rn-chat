8bec79af7447354cbafba8eadc1ea2d511513b6e
danilichev <v.danilichev@gmail.com>
2024-08-01 18:05:27 +0300

    Init
    Generated by create-expo-app 3.0.0.

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  extends: "expo",
+  plugins: ["prettier", "simple-import-sort", "import"],
+  rules: {
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
+    "simple-import-sort/exports": "error",
+    "simple-import-sort/imports": "error",
+  },
+  overrides: [
+    {
+      files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
+      rules: {
+        "simple-import-sort/imports": [
+          "error",
+          {
+            groups: [["^\\u0000"], ["^@?\\w"], ["^src/"], ["^\\."]],
+          },
+        ],
+      },
+    },
+  ],
+};
---
+++ src/app/index.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: "#fff",
+    flex: 1,
+    justifyContent: "center",
+  },
+});
