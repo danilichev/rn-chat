75270f85114d4119b0d5e970e860c7a4cbec4dfa
danilichev <v.danilichev@gmail.com>
2024-08-09 20:51:16 +0300

    Update StartChat screen

--- src/app/(app)/start-chat.tsx
+++ src/app/(app)/start-chat.tsx
@@ -2,15 +2,20 @@ import { useCallback } from "react";
 import { FlatList, ListRenderItem, StyleSheet, View } from "react-native";
 
 import { getUsers } from "src/api/users";
+import { ListFooter } from "src/components/ListFooter";
 import { UserListItem } from "src/components/UserListItem";
+import { useNotifyOnChangeProps } from "src/hooks/useNotifyOnChangeProps";
 import { usePaginationQuery } from "src/hooks/usePaginationQuery";
 import { User } from "src/types/domain";
 import { keyExtractor } from "src/utils/common";
 
-const USERS_PER_REQUEST = 15;
+const USERS_PER_REQUEST = 20;
 
 export default function StartChat() {
-  const { data, loadMore } = usePaginationQuery<User>({
+  const notifyOnChangeProps = useNotifyOnChangeProps();
+
+  const { data, isFetching, loadMore } = usePaginationQuery<User>({
+    notifyOnChangeProps,
     queryFn: getUsers,
     queryKey: ["users"],
     variables: { limit: USERS_PER_REQUEST },
@@ -24,11 +29,12 @@ export default function StartChat() {
   return (
     <View style={styles.container}>
       <FlatList
+        ListFooterComponent={<ListFooter isLoading={true} />}
         data={data}
         initialNumToRender={USERS_PER_REQUEST}
         keyExtractor={keyExtractor}
-        onEndReached={loadMore}
-        onEndReachedThreshold={0.5}
+        onEndReached={isFetching ? undefined : loadMore}
+        onEndReachedThreshold={0.25}
         renderItem={renderItem}
       />
     </View>
