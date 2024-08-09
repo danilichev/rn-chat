4fa8a5b8b57476e93026cb253486d5cba184c382
danilichev <v.danilichev@gmail.com>
2024-08-09 09:55:38 +0300

    Make getUsers query paginable

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,13 +1,23 @@
 import { supabase } from "src/infra/supabase";
+import { Pagination, PaginationResult } from "src/types/common";
 import { User } from "src/types/domain";
 import { dbUserToUser } from "src/utils/mappers";
 
-export const getUsers = async (): Promise<User[]> => {
-  const { data, error } = await supabase
+export const getUsers = async ({
+  limit,
+  offset = 0,
+}: Pagination): Promise<PaginationResult<User>> => {
+  const { count, data, error } = await supabase
     .from("users")
-    .select(`avatar_url, email, full_name, id`);
+    .select(`avatar_url, email, full_name, id`, { count: "exact" })
+    .range(offset, offset + limit - 1);
 
   if (error) throw error;
 
-  return data.map((i) => dbUserToUser(i));
+  return {
+    data: data.map(dbUserToUser),
+    limit,
+    offset,
+    total: count || 0,
+  };
 };
