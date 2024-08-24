4db694e204bbb9e9c62cd59bbbf53bf513694976
danilichev <v.danilichev@gmail.com>
2024-08-24 23:21:08 +0300

    Setup android build

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,14 @@
 import { Slot } from "expo-router";
+import { KeyboardProvider } from "react-native-keyboard-controller";
 
 import { QueryClientProvider } from "src/services/queryClient";
 
 export default function RootLayout() {
   return (
-    <QueryClientProvider>
-      <Slot />
-    </QueryClientProvider>
+    <KeyboardProvider>
+      <QueryClientProvider>
+        <Slot />
+      </QueryClientProvider>
+    </KeyboardProvider>
   );
 }
